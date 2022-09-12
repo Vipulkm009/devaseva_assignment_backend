1e64596d6093f112e3b84ea6204824e7921b952d
Vipulmaurya009 <vipul.maurya@iesoftek.com>
2022-09-12 12:34:07 +0530

    Fixed readAll

--- controllers/readData.js
+++ controllers/readData.js
@@ -24,14 +24,14 @@ exports.readVegetables = (req, res) => {
 };
 
 exports.readAll = (req, res) => {
-    var data;
-    Fruit.find().exec((err, fruits) => {
+    var fruits;
+    Fruit.find().exec((err, data) => {
         if(err) {
             return res.status(400).json({
                 error: "NO data in DB."
             });
         }
-        data = fruits;
+        fruits = data;
     });
     Vegetable.find().exec((err, vegetables) => {
         if(err) {
